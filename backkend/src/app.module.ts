9381f8f3fd56ec260990daa60425dd2340c0c31c
hieuthai <hieuthai642@gmail.com>
2022-06-14 20:42:10 +0700

    feat: add typeorm

--- backkend/src/app.module.ts
+++ backkend/src/app.module.ts
@@ -3,11 +3,23 @@ import { AutomapperModule } from '@automapper/nestjs';
 import { classes } from '@automapper/classes';
 import { AutomapperUserProfile } from './v1/shared/AutomapperUserProfile';
 import { UsersModule } from './v1/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './v1/users/users.entity';
 
 @Module({
   imports: [
     AutomapperModule.forRoot({
       strategyInitializer: classes(),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 54320,
+      username: 'dev',
+      password: '1234',
+      database: 'demo-db',
+      entities: [User],
+      synchronize: true,
     }),
     UsersModule,
   ],
