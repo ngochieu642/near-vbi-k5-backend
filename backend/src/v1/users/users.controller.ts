ce310949fa83408714269068f44844c578bb439c
hieuthai <hieuthai642@gmail.com>
2022-06-15 14:31:26 +0700

    feat: replace console with logger

--- backend/src/v1/users/users.controller.ts
+++ backend/src/v1/users/users.controller.ts
@@ -1,25 +1,29 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Param, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/CreateUserDto';
 import { UsersService } from './users.service';
 import { CreateIdentityRequestDto } from './dtos/CreateIdentityRequestDto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
+  private logger: Logger;
+
+  constructor(private userService: UsersService) {
+    this.logger = new Logger(UsersController.name);
+  }
 
   @Post('/signup')
   async createUser(@Body() body: CreateUserDto): Promise<void> {
     const response = await this.userService.create(body.email, body.password);
-    console.log(response);
+    this.logger.log(response);
   }
 
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    console.log(id);
+    this.logger.log(id);
   }
 
   @Post('/identity-request')
   async createIdentityRequest(@Body() body: CreateIdentityRequestDto): Promise<void> {
-    console.log(body);
+    this.logger.log(body);
   }
 }
