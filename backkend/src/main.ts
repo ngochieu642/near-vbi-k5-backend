9c9c8b4bd473cf9b95b904c188a0bc36f1dcd4b8
hieuthai <hieuthai642@gmail.com>
2022-06-15 10:54:10 +0700

    feat: add all exception handler

--- backkend/src/main.ts
+++ backkend/src/main.ts
@@ -1,13 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
-import { AppModule } from './app.module';
-import { V1Constants } from './v1/V1Constants';
+import {HttpAdapterHost, NestFactory} from '@nestjs/core';
+import {Logger} from '@nestjs/common';
+import {AppModule} from './app.module';
+import {V1Constants} from './v1/V1Constants';
+import {AllExceptionsFilter} from "./v1/nest-utils/all-exceptions.filter";
 
 const logger = new Logger('main');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Prefix
   app.setGlobalPrefix(V1Constants.API_PREFIX);
+
+  // Filters
+  const httpAdapter = app.get<HttpAdapterHost>(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   await app.listen(3000);
 }
 
