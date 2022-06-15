2114f9dd18de9867e25b6a7086b05011d9181a3c
hieuthai <hieuthai642@gmail.com>
2022-06-15 14:18:13 +0700

    chore: add requried not empty

--- backend/src/v1/users/dtos/CreateIdentityRequestDto.ts
+++ backend/src/v1/users/dtos/CreateIdentityRequestDto.ts
@@ -1,34 +1,44 @@
 import { Type } from 'class-transformer';
-import { IsArray, IsISO8601, IsString } from 'class-validator';
+import { IsArray, IsISO8601, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateIdentityRequestDto {
   @IsString()
+  @IsNotEmpty()
   accountId: string;
 
   @IsString()
+  @IsNotEmpty()
   userPublicKey: string;
 
   @IsString()
+  @IsNotEmpty()
   name: string;
 
   @IsString()
+  @IsNotEmpty()
   gender: string;
 
   @IsISO8601()
+  @IsNotEmpty()
   dob: string;
 
   @IsString()
+  @IsNotEmpty()
   address: string;
 
   @IsString()
+  @IsNotEmpty()
   ccid: string;
 
   @IsString()
+  @IsNotEmpty()
   phoneNumber: string;
 
   @IsString()
+  @IsNotEmpty()
   nationality: string;
 
+  @IsNotEmpty()
   @IsArray({ each: true })
   @Type(() => Array)
   faceVector: string;
