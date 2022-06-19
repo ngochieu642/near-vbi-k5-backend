3d09595ec6120fcc19ee8c7f63be26bd768d810b
Ted <dqkhoa02@gmail.com>
2022-06-19 17:36:16 +0700

    [NEAR][FE] Update Config

--- frontend/src/services/axiosInstance.ts
+++ frontend/src/services/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api/v1'
+    baseURL: 'http://54.254.178.184:30000/api/v1'
 });
 
 export default axiosInstance;
